74578257c34c9eedf43677d107078e01b16110b9
Steven BENET <sbenet@opnf.fr>
2019-02-01 16:26:39 +0100

    update code

--- listing.sql
+++ listing.sql
@@ -1,4 +1,7 @@
-select 
+select niveau, role, firstname, lastname, abo
+from (
+
+select
 concat(
 	substring(c.jour,1, 2),
 	case when c.type_cours = "Hip Hop" then "HH"
@@ -19,7 +22,8 @@ case
 		case when cmd.id_user2 is null then '' else 'D' end,
 		case when cmd.illimite = 1 then 'I' else '' end,
 		case when cmd.reduit = 1 then 'R' else '' end
-) as abo
+) as abo,
+c.id
 FROM `users`
 inner join inscription_cours ic on users.id = ic.id_user
 inner join cours c on c.id = ic.id_cours 
@@ -27,6 +31,42 @@ inner join commande cmd on users.id in (cmd.id_user1, cmd.id_user2)
 where ic.validite >= CURRENT_DATE
 and 'annule' not in (ic.status, cmd.status)
 and cmd.validite >= CURRENT_DATE
-and c.id <> 9
-order by c.id, ic.role, users.firstName, users.lastname, abo
+and c.id not in (9, 10)
+
+union
+
+select
+concat(
+	substring(c.jour,1, 2),
+	c.type_cours,
+	"_",
+	c.niveau
+) as niveau,
+ic.role,
+users.firstname,
+users.lastname,
+concat(
+case
+            when cmd.validite = date '2019-07-31' then 'A'
+            when cmd.validite = date '2019-03-31' then 'T'
+            else ''
+        end,
+		case when cmd.id_user2 is null then '' else 'D' end,
+		case when cmd.illimite = 1 then 'I' else '' end,
+		case when cmd.reduit = 1 then 'R' else '' end,
+		case when cmd.id is null then '10' else '' end
+) as abo,
+c.id
+FROM `users`
+inner join inscription_cours ic on users.id = ic.id_user
+inner join cours c on c.id = ic.id_cours
+left outer join commande cmd on users.id in (cmd.id_user1, cmd.id_user2)
+where ic.validite >= CURRENT_DATE
+and 'annule' not in (ic.status, coalesce(cmd.status, ''))
+and coalesce(cmd.validite, CURRENT_DATE) >= CURRENT_DATE
+and c.id  = 10
+) union_query
+
+order by id, role, firstName, lastname, abo
+
 ;
